41d29ae5cd97dac6796356dd0aaca3a880a23f5e
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-09-19 00:23:24 +0300

    added section products

--- gulpfile.js
+++ gulpfile.js
@@ -19,16 +19,6 @@ gulp.task('sass', function(){
           .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('script', function(){
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-  ])
-      .pipe(concat('libs.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('app/js'))
-});
-
 gulp.task('style', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
@@ -38,6 +28,17 @@ gulp.task('style', function(){
       .pipe(concat('libs.min.css'))
       .pipe(cssmin())
       .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('script', function(){
+  return gulp.src([
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/mixitup/dist/mixitup.js'
+  ])
+      .pipe(concat('libs.min.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('app/js'))
 });
 
 gulp.task('html', function(){
