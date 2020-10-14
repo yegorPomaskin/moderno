53d913b404214e63a2cdf247afce89311f2f7a71
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-10-14 11:51:08 +0300

    adaptive header/footer

--- app/js/main.js
+++ app/js/main.js
@@ -32,6 +32,14 @@ $(function(){
     $('.icon-th-large').addClass('active');
     $('.icon-th-list').removeClass('active');
   });
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
 
   var mixer = mixitup('.products__inner-box');
 
