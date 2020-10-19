95a6fae3a78ac232a498db6bee389be790645d26
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-10-19 13:57:11 +0300

    added 404, about, contacts

--- app/js/main.js
+++ app/js/main.js
@@ -32,6 +32,15 @@ $(function(){
     $('.icon-th-large').addClass('active');
     $('.icon-th-list').removeClass('active');
   });
+
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
   
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
@@ -40,6 +49,8 @@ $(function(){
   $('.header__btn-menu').on('click', function(){
     $('.header__box').toggleClass('active');
   });
+
+  
 
   var mixer = mixitup('.products__inner-box');
 
