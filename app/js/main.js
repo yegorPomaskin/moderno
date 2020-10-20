c63d430e8dcf256cea4c60e8a58bc0202e88b570
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-10-20 20:36:41 +0300

    added upload

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,8 @@ $(function(){
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
     });
+
+  $('input[type="file"], select').styler(); 
   
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
